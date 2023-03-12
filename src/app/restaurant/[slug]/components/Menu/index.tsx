278aa6a613c7f0b45685b2b9451094eb72aaa122
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2023-03-12 14:37:16 +0200

    feat: reserve page into components

---
+++ src/app/restaurant/[slug]/components/Menu/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import MenuCard from "../MenuCard";
+
+type Props = {};
+
+const Menu = ({}: Props): JSX.Element => {
+  return (
+    <div className="bg-white mt-5">
+      <div>
+        <div className="mt-4 pb-1 mb-1">
+          <h1 className="font-bold text-4xl">Menu</h1>
+        </div>
+        <div className="flex flex-wrap justify-between">
+          <MenuCard />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
